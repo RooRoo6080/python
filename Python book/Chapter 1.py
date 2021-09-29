5a98ae865b671a8657cc6e91a14f87438fb390bd
RooRoo6080 <67977174+RooRoo6080@users.noreply.github.com>
2021-09-28 17:27:34 -0700

    Update Chapter 1.py

--- Python book/Chapter 1.py
+++ Python book/Chapter 1.py
@@ -1,7 +1,9 @@
 print 60 * 24 * 7
 
 print "------------------------------"
-print "My name is Reuel Joseph"
-print "I was born on May 24, 2008"
+print "My name is ***** ******"
+print "I was born on **** **, ****"
 print "My favorite color is red"
 print "this is my test"
+
+#forget this program exists
